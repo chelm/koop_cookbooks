e5eb915cc5934939867c3df9311b233ac70dff25
chelm <christopher.helm@gmail.com>
2014-03-30 09:07:46 -0600

    adding back template

--- koop_app_server/recipes/default.rb
+++ koop_app_server/recipes/default.rb
@@ -1,28 +1,40 @@
 include_recipe 'apt'
+include_recipe 'deploy'
 
-apt_repository 'apt.postgresql.org' do
-  components ['main']
-  distribution "#{node['lsb']['codename']}-pgdg"
-  key 'http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc'
-  notifies :run, 'execute[apt-get update]', :immediately
-  uri 'http://apt.postgresql.org/pub/repos/apt'
+node[:deploy].each do |application, deploy|
+
+  apt_repository 'apt.postgresql.org' do
+    components ['main']
+    distribution "#{node['lsb']['codename']}-pgdg"
+    key 'http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc'
+    notifies :run, 'execute[apt-get update]', :immediately
+    uri 'http://apt.postgresql.org/pub/repos/apt'
+  end
+  
+  package 'pkg-config'
+  package 'libcairo2-dev'
+  package "pgdg-keyring"
+  package 'libgdal-dev'
+  package 'libpq-dev'
+  package 'postgresql-9.3'
+  package 'postgresql-client-9.3'
+  package 'postgresql-server-dev-all'
+  
+  directory node[:koop][:data_dir] do
+    mode 0755
+    action :create
+  end
+
+  template 'local.js' do
+    path "#{deploy[:deploy_to]}/local.js"
+    source 'local.js.erb'
+    owner 'root'
+    group 'root'
+    mode 0644
+  end
+
 end
 
-package 'pkg-config'
-package 'libcairo2-dev'
-package "pgdg-keyring"
-package 'libgdal-dev'
-package 'libpq-dev'
-package 'postgresql-9.3'
-package 'postgresql-client-9.3'
-package 'postgresql-server-dev-all'
-
-directory node[:koop][:data_dir] do
-  mode 0755
-  action :create
-end
-
-#node[:deploy].each do |application, deploy|
 #
 #  execute 'apt-get update' do
 #    command 'apt-get update'
