d5dae6c5b81059776cecd75c412ef5e0d58f07ca
chelm <christopher.helm@gmail.com>
2014-03-31 09:53:41 -0600

    adding a path to attributes

--- koop_app_server/attributes/default.rb
+++ koop_app_server/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:koop][:gh_token] = '123'
 default[:koop][:logfile] = '/mnt/koop/koop-log.log'
-default[:koop][:data_dir] = '/mnt/koop/'
+default[:koop][:data_dir] = '/mnt/koop/data'
 default[:koop][:db][:postgis][:conn] = "postgres://localhost/koop"
 default[:koop][:port] = 80
