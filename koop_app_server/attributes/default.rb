a2be7978c3767f940c3e70db480d2fa6eac4a638
chelm <christopher.helm@gmail.com>
2014-04-01 12:50:49 -0600

    adding s3 to config

--- koop_app_server/attributes/default.rb
+++ koop_app_server/attributes/default.rb
@@ -3,3 +3,6 @@ default[:koop][:logfile] = '/mnt/koop/koop-log.log'
 default[:koop][:data_dir] = '/mnt/koop/data'
 default[:koop][:db][:postgis][:conn] = "postgres://localhost/koop"
 default[:koop][:port] = 80
+default[:koop][:aws_key_id] = 'akid'
+default[:koop][:aws_secret_access_key] = 'sekritkey'
+default[:koop][:aws_region] = 'us-west-2'
