fc33d2b0b265d899681ecd62e10c86a2ec0d0b9a
chelm <christopher.helm@gmail.com>
2014-04-01 14:45:12 -0600

    getting rid of the recipe to debug opsworks

--- koop_app_server/recipes/koop_deploy.rb
+++ koop_app_server/recipes/koop_deploy.rb
@@ -3,17 +3,17 @@
 #  action :nothing
 #end
 
-directory node[:koop][:data_dir] do
-  mode 0755
-  action :create
-end
-
-template 'local.js' do
-  cookbook 'koop_app_server'
-  path "/srv/www/koop/current/config/local.js"
-  source 'local.js.erb'
-  owner 'root'
-  group 'root'
-  mode 0644
-end
+#directory node[:koop][:data_dir] do
+#  mode 0755
+#  action :create
+#end
+#
+#template 'local.js' do
+#  cookbook 'koop_app_server'
+#  path "/srv/www/koop/current/config/local.js"
+#  source 'local.js.erb'
+#  owner 'root'
+#  group 'root'
+#  mode 0644
+#end
 
