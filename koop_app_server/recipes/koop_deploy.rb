fab323ac232744053b0d6e5cd9d3f8a23a816234
chelm <christopher.helm@gmail.com>
2014-04-02 11:58:35 -0600

    commenting out

--- koop_app_server/recipes/koop_deploy.rb
+++ koop_app_server/recipes/koop_deploy.rb
@@ -1,19 +1,19 @@
-#execute 'npm install -g grunt-cli forever' do
-#  command 'npm install -g grunt-cli forever'
-#  action :nothing
-#end
+execute 'npm install -g grunt-cli forever' do
+  command 'npm install -g grunt-cli forever'
+  action :nothing
+end
 
-#directory node[:koop][:data_dir] do
-#  mode 0755
-#  action :create
-#end
-#
-#template 'local.js' do
-#  cookbook 'koop_app_server'
-#  path "/srv/www/koop/current/config/local.js"
-#  source 'local.js.erb'
-#  owner 'root'
-#  group 'root'
-#  mode 0644
-#end
+directory node[:koop][:data_dir] do
+  mode 0755
+  action :create
+end
+
+template 'local.js' do
+  cookbook 'koop_app_server'
+  path "/srv/www/koop/current/config/local.js"
+  source 'local.js.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+end
 
