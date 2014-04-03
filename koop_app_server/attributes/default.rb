dab1fa89fd5ce9b7f0fe987c0776bdaa661cf007
chelm <christopher.helm@gmail.com>
2014-04-03 15:49:24 -0600

    setting s3 bucket attribute

--- koop_app_server/attributes/default.rb
+++ koop_app_server/attributes/default.rb
@@ -1,5 +1,6 @@
 default[:koop][:gh_token] = '123'
 default[:koop][:logfile] = '/mnt/koop/koop-log.log'
+default[:koop][:s3_bucket] = 'koopdev'
 default[:koop][:data_dir] = '/mnt/koop/data'
 default[:koop][:db][:postgis][:conn] = "postgres://localhost/koop"
 default[:koop][:port] = 80
