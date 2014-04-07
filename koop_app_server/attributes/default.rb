d1e6798eb6541efb869c95401dd1e8a1139a09fc
chelm <christopher.helm@gmail.com>
2014-04-07 08:03:00 -0600

    fixing aws region

--- koop_app_server/attributes/default.rb
+++ koop_app_server/attributes/default.rb
@@ -6,4 +6,4 @@ default[:koop][:db][:postgis][:conn] = "postgres://localhost/koop"
 default[:koop][:port] = 80
 default[:koop][:aws_key_id] = 'akid'
 default[:koop][:aws_secret_access_key] = 'sekritkey'
-default[:koop][:aws_region] = 'us-west-2'
+default[:koop][:aws_region] = 'us-east-1'
