e4b9c7245300a0e526a156bbabdaba33e8639d43
chelm <christopher.helm@gmail.com>
2014-04-28 15:38:07 -0600

    updating

--- koop_app_server/recipes/koop_deploy.rb
+++ koop_app_server/recipes/koop_deploy.rb
@@ -1,3 +1,5 @@
+include_recipe 'deploy'
+
 execute 'npm install -g grunt-cli forever' do
   command 'sudo npm install -g grunt-cli forever'
   ignore_failure false
@@ -8,18 +10,41 @@ directory node[:koop][:data_dir] do
   action :create
 end
 
-template 'local.js' do
-  cookbook 'koop_app_server'
-  path "/srv/www/koop/current/config/local.js"
-  source 'local.js.erb'
-  owner 'root'
-  group 'root'
-  mode 0644
-end
+  template 'defaul.yml' do
+    cookbook 'koop_app_server'
+    path "/srv/www/koop/current/config/default.yml"
+    source 'default.yml.erb'
+    owner 'root'
+    group 'root'
+    mode 0644
+  end
 
-execute 'compile assets' do
-  cwd "/srv/www/koop/current"
-  command 'sudo grunt compileAssets'
-  ignore_failure false
-end
+  execute 'install gist' do
+    cwd "/srv/www/koop/current"
+    command 'sudo npm install https://github.com/chelm/koop-gist/tarball/master'
+    ignore_failure false
+  end
 
+  execute 'install github' do
+    cwd "/srv/www/koop/current"
+    command 'sudo npm install https://github.com/chelm/koop-github/tarball/master'
+    ignore_failure false
+  end
+  
+  execute 'install socrata' do
+    cwd "/srv/www/koop/current"
+    command 'sudo npm install https://github.com/chelm/koop-socrata/tarball/master'
+    ignore_failure false
+  end
+
+  execute 'install agol' do
+    cwd "/srv/www/koop/current"
+    command 'sudo npm install https://github.com/chelm/koop-agol/tarball/master'
+    ignore_failure false
+  end
+
+  execute 'install geocommons' do
+    cwd "/srv/www/koop/current"
+    command 'sudo npm install https://github.com/chelm/koop-geocommons/tarball/master'
+    ignore_failure false
+  end
