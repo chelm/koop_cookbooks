b219d4b36431c42299519edc2bc65c6fdce0497d
chelm <christopher.helm@gmail.com>
2014-10-20 20:01:25 -0600

    adding ckan

--- koop_app_server/recipes/koop_deploy.rb
+++ koop_app_server/recipes/koop_deploy.rb
@@ -75,6 +75,12 @@ node[:deploy].each do |application, deploy|
     ignore_failure false
   end
 
+  execute 'install ckan' do
+    cwd "#{deploy[:current_path]}"
+    command 'sudo npm install https://github.com/chelm/koop-ckan/tarball/master'
+    ignore_failure false
+  end
+
   ruby_block "restart node.js application #{application}" do
     block do
       Chef::Log.info("restart node.js via: #{node[:deploy][application][:nodejs][:restart_command]}")
