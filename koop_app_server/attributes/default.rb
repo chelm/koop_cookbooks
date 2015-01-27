c9e574e359e7604b12daef17ba5b43ed8bca5a57
chelm <christopher.helm@gmail.com>
2015-01-27 13:01:18 -0600

    fixing data dir path

--- koop_app_server/attributes/default.rb
+++ koop_app_server/attributes/default.rb
@@ -1,7 +1,7 @@
 default[:koop][:gh_token] = '123'
 default[:koop][:logfile] = '/mnt/koop/koop-log.log'
 default[:koop][:s3][:bucket] = 'koop-development-public-downloads'
-default[:koop][:data_dir] = '/mnt/koop/data'
+default[:koop][:data_dir] = '/mnt/koop/data/'
 default[:koop][:db][:postgis][:conn] = "postgres://localhost/koop"
 default[:koop][:port] = 80
 default[:koop][:agol][:request_workers][:host] = "127.0.0.1"
